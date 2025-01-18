2ec3b442ed752a9c0dfd46792f35a5e03d3980ac
k0789789 <k0789789@naver.com>
2025-01-19 04:19:14 +0900

    Feat : Add kakao request dto

---
+++ src/main/java/yjkim/GuideUs/Route/DTO/CalculateTimeKakaoRequest.java
@@ -0,0 +1,54 @@
+package yjkim.GuideUs.Route.DTO;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class CalculateTimeKakaoRequest {
+
+    private String priority;
+    private String[] avoid;
+    private int roadevent;
+    private boolean alternatives;
+    private boolean road_details;
+    private int car_type;
+    private String car_fuel;
+    private boolean car_hipass;
+    private boolean summary;
+    private Origin origin;
+    private Destination destination;
+    private Waypoint[] waypoints;
+
+    @Getter
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    public static class Origin{
+        private String name;
+        private String x;
+        private String y;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    public static class Destination{
+        private String name;
+        private String x;
+        private String y;
+    }
+    @Getter
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    public static class Waypoint{
+        private String name;
+        private String x;
+        private String y;
+
+    }
+
+
+}

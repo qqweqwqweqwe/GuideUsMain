e4411b13cd93bbacdc5fd300d4c87b6df7a1f62e
k0789789 <k0789789@naver.com>
2025-01-19 00:07:41 +0900

    Feat : Add kafka listner for consumer

--- src/main/java/yjkim/GuideUs/Kafka/Service/KafkaService.java
+++ src/main/java/yjkim/GuideUs/Kafka/Service/KafkaService.java
@@ -1,13 +1,21 @@
 package yjkim.GuideUs.Kafka.Service;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import yjkim.GuideUs.Route.DTO.RouteCalcualteRequest;
 
 import java.util.Properties;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaService {
+
+    private final KafkaTemplate<String,RouteCalcualteRequest> kafkaTemplate;
+    public static final String ROUTE_CALCULATE_KAFKA_TOPIC = "route-calculate";
 
 
     public KafkaProducer<String, String> createProducer(){
@@ -25,6 +33,16 @@ public class KafkaService {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         return new KafkaConsumer<>(props);
+    }
+    public void send(String topic, String key, RouteCalcualteRequest routeCalcualteRequest){
+        kafkaTemplate.send(topic,key,routeCalcualteRequest);
+        return;
+    }
 
+    @KafkaListener(topics = ROUTE_CALCULATE_KAFKA_TOPIC )
+    public void consume(RouteCalcualteRequest routeCalcualteRequest){
+
+        // 리스너에서 계산하고 해당 결과 redis에 저장
+        //
     }
 }

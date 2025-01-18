51772da89850516e88c87690688fdac0bfe6f2e6
k0789789 <k0789789@naver.com>
2025-01-19 06:42:32 +0900

    Feat : Add NoArgsConstructor

--- src/main/java/yjkim/GuideUs/Route/DTO/RouteCalcualteRequest.java
+++ src/main/java/yjkim/GuideUs/Route/DTO/RouteCalcualteRequest.java
@@ -1,10 +1,13 @@
 package yjkim.GuideUs.Route.DTO;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RouteCalcualteRequest {
 
     // todo 나중에 카카오 장소 정보로 리팩할것

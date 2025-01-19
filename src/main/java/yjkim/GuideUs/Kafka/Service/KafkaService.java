0af908dff0cd195dbb09a66d2a45f48812751927
k0789789 <k0789789@naver.com>
2025-01-20 01:38:18 +0900

    Feat : Add zookeeper starter and kafka starter

--- src/main/java/yjkim/GuideUs/Kafka/Service/KafkaService.java
+++ src/main/java/yjkim/GuideUs/Kafka/Service/KafkaService.java
@@ -1,15 +1,21 @@
 package yjkim.GuideUs.Kafka.Service;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import yjkim.GuideUs.Route.DTO.RouteCalcualteRequest;
 import yjkim.GuideUs.Route.Service.RouteService;
 
+import javax.sound.sampled.Port;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 @Service
@@ -18,24 +24,68 @@ public class KafkaService {
 
     private final KafkaTemplate<String,RouteCalcualteRequest> kafkaTemplate;
     private final RouteService routeService;
-    public static final String ROUTE_CALCULATE_KAFKA_TOPIC = "route-calculate";
+    public static final String ROUTE_CALCULATE_KAFKA_TOPIC = "guide-us-route";
+    private Process zookeeperProcess;
+    private Process kafkaProcess;
 
 
-    public KafkaProducer<String, String> createProducer(){
-        Properties configs = new Properties();
-        configs.put("bootstrap.servers", "localhost:9092"); // Kafka 브로커 주소
-        configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        return new KafkaProducer<>(configs);
+    // 어플리케이션 실행하면 자동으로 주키퍼랑 카프카 실행하도록
+    // 그리고 dummy-data 보냄으로써 카프카랑 연결까지 하도록
+    @EventListener(ApplicationStartedEvent.class)
+    public void onApplicationStart(){
+        startZookeeper();
+        startKafka();
+        RouteCalcualteRequest routeCalcualteRequest = new RouteCalcualteRequest();
+        routeCalcualteRequest.setDep(new String[]{"test","test","test"});
+        routeCalcualteRequest.setDes(new String[]{"test","test","test"});
+
+        this.send(ROUTE_CALCULATE_KAFKA_TOPIC, "test",routeCalcualteRequest);
     }
 
-    public KafkaConsumer<String, String> createConsumer(){
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "json-consumer-group");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        return new KafkaConsumer<>(props);
+
+    public void startZookeeper() {
+        try {
+            String command = "bin\\windows\\zookeeper-server-start.bat config\\zookeeper.properties";
+            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/k", command);
+            processBuilder.directory(new File("C:\\kafka_2.13-3.9.0")); // Kafka 설치 경로
+            zookeeperProcess = processBuilder.start();
+            System.out.println("Zookeeper started!");
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                if (zookeeperProcess != null && zookeeperProcess.isAlive()) {
+                    System.out.println("Shutting down Zookeeper...");
+                    zookeeperProcess.destroy();
+
+                }
+
+            }));
+
+            System.out.println("Zookeeper started. Press Ctrl+C to stop the server.");
+        }
+        catch (Exception e){
+            System.out.println("Zookeeper error  : " + e);
+        }
+    }
+
+    private void startKafka() {
+        try {
+            String command = "bin\\windows\\kafka-server-start.bat config\\server.properties";
+            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", command);
+            processBuilder.directory(new File("C:\\kafka_2.13-3.9.0")); // Kafka 설치 경로
+            kafkaProcess = processBuilder.start();
+            System.out.println("Kafka started!");
+            // 서버 종료시 자동으로 카프카 종료되도록
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                if (kafkaProcess != null && kafkaProcess.isAlive()) {
+                    System.out.println("Shutting down Zookeeper...");
+                    kafkaProcess.destroy();
+                }
+            }));
+        }
+
+        catch (Exception e){
+            System.out.println("Kafka error  : " + e);
+        }
     }
     public void send(String topic, String key, RouteCalcualteRequest routeCalcualteRequest){
         kafkaTemplate.send(topic,key,routeCalcualteRequest);
@@ -44,13 +94,25 @@ public class KafkaService {
 
     @KafkaListener(topics = ROUTE_CALCULATE_KAFKA_TOPIC )
     public void consume(ConsumerRecord<String, RouteCalcualteRequest> record){
+
         String key = record.key();
+        System.out.println("key : " + key);
         RouteCalcualteRequest routeCalcualteRequest = record.value();
         String[] dep = routeCalcualteRequest.getDep();
         String[] des = routeCalcualteRequest.getDes();
         String[][] trans = routeCalcualteRequest.getTrans();
-        trans = this.routeService.calculateMinimumTimeRoute(dep,des,trans);
 
+        System.out.println(dep[0]);
+        System.out.println(des[0]);
+
+        try {
+            trans = this.routeService.calculateMinimumTimeRoute(dep, des, trans);
+        }
+        catch (Exception e){
+            System.out.println("E : " + e);
+        }
+
+//
         // 레디스에 저장
 
 

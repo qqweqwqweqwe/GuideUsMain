1f3a4e30ae103eb2a996eb86c5bc2ce95b883a69
k0789789 <k0789789@naver.com>
2025-01-20 01:40:15 +0900

    Feat : Add no args

--- src/main/java/yjkim/GuideUs/Route/DTO/RouteCalculateResponse.java
+++ src/main/java/yjkim/GuideUs/Route/DTO/RouteCalculateResponse.java
@@ -2,10 +2,12 @@ package yjkim.GuideUs.Route.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RouteCalculateResponse {
 
 

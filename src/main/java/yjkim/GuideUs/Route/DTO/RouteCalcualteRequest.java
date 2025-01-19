e8840df44edeebe8995acd0ef2551c29b863e899
k0789789 <k0789789@naver.com>
2025-01-20 01:39:41 +0900

    Fix : Set default value

--- src/main/java/yjkim/GuideUs/Route/DTO/RouteCalcualteRequest.java
+++ src/main/java/yjkim/GuideUs/Route/DTO/RouteCalcualteRequest.java
@@ -4,15 +4,19 @@ import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class RouteCalcualteRequest {
 
     // todo 나중에 카카오 장소 정보로 리팩할것
     private String[] dep;
     private String[] des;
-    private String[][] trans;
+
+    // 기본 생성자로 생성할 떄 null 방지
+    private String[][] trans= new String[][]{};
 
 }

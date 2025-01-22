e90e7f5273c46eb35c9f1a6671ad0706c9609f12
k0789789 <k0789789@naver.com>
2025-01-22 18:46:07 +0900

    Feat : Add redis get

--- src/main/java/yjkim/GuideUs/Redis/Service/RedisService.java
+++ src/main/java/yjkim/GuideUs/Redis/Service/RedisService.java
@@ -1,11 +1,15 @@
 package yjkim.GuideUs.Redis.Service;
 
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RedisService {
 
     private RedisTemplate<String,String> redisTemplate;
@@ -15,5 +19,11 @@ public class RedisService {
 
     public void save(String key, String value){
         redisTemplate.opsForValue().set(key,value);
+    }
+
+    public String get(String key){
+
+        return redisTemplate.opsForValue().get(key);
+
     }
 }

7073588bda2a289784d405cf573c43ce25f1330b
k0789789 <k0789789@naver.com>
2025-01-22 18:45:36 +0900

    Feat : Add result

--- src/main/java/yjkim/GuideUs/Maps/Controller/MapsController.java
+++ src/main/java/yjkim/GuideUs/Maps/Controller/MapsController.java
@@ -1,8 +1,11 @@
 package yjkim.GuideUs.Maps.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,10 +23,13 @@ public class MapsController {
     }
 
     // 결과를 출력해주는 컨트롤러임
-    @PostMapping("/result")
-    public String getResult (Model model){
-
+    @GetMapping("/result/{requestId}")
+    public String calculateShortestRoute(
+            @PathVariable String requestId
+    ){
+        System.out.println(requestId);
         return "result";
+
     }
 
 }
